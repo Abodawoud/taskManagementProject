9388bbea777d12cb70bcd0be597f978347e9906b
Abodawoud <yousefabodawoud@gmail.com>
2025-04-06 01:59:15 +0200

    feat: introducing inputs/outputs decorators

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -7,12 +7,26 @@ import { Component, Input } from '@angular/core';
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  @Input({ required: true }) avatar: unknown;
-  @Input({ required: true }) name: unknown;
+  /*
+  - To use input() signals
+  -- avatar = input<string>();
+  - If u want it required
+  -- avatar = input.required<string>();
+
+  - To use output() signals
+  -- select = output<string>(); // don't forget to put <string>
+  - output() signals are emitted behind the scenes
+  */
+  @Input({ required: true }) id!: string;
+  @Input({ required: true }) avatar!: string;
+  @Input({ required: true }) name!: string;
+  @Output() select = new EventEmitter<string>();
 
   get imagePath() {
     return `images/users/${this.avatar}`;
   }
 
-  onSelectUser() {}
+  onSelectUser() {
+    this.select.emit(this.id);
+  }
 }
